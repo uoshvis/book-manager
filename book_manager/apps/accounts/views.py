1f5a7d2a1b6f5ba9da6b8c66dfe472d4e58c657a
uoshvis <uoshvis@gmail.com>
2021-08-14 14:36:26 +0300

    add main_page and register views

--- book_manager/apps/accounts/views.py
+++ book_manager/apps/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
-# Create your views here.
+
+def main_page(request):
+    return render(request=request, template_name='accounts/main.html')
+
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("accounts:main_page")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="accounts/register.html", context={"register_form": form})
